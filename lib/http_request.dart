da72817d4fc361c8e30d1d1fd1dfb64976648e5a
eneskaya96 <enes.kaya95@hotmail.com>
2022-04-09 18:49:16 +0300

    http request refactored

--- lib/http_request.dart
+++ lib/http_request.dart
@@ -4,149 +4,59 @@ import 'package:http/http.dart' as http;
 
 String base_url = 'http://127.0.0.1:5000';
 
-Future<http.Response> send_request() async {
-  final response = await http.get(
-    Uri.parse(base_url),
-  );
+Future<String> send_request(body, end_point,  is_post) async {
+  final http.Response response;
+  String url = base_url + end_point;
 
-  if (response.statusCode == 200) {
-    // If the server did return a 201 CREATED response,
-    // then parse the JSON.
-    return response;
-  } else {
-    throw Exception('Failed HTTP REQUEST');
+  if(is_post){
+    response = await http.get(Uri.parse(url),);
   }
+  else {
+    response = await http.post(Uri.parse(url),body: body);
+  }
+
+  if (response.statusCode == 200) { return response.body;}
+  else {throw Exception('Failed HTTP REQUEST');}
 }
 
 
 Future<String> send_login_request(email, password) async {
-
   var body = jsonEncode({ 'email': email,
                           'password': password} );
-
-  final response = await http.post(
-      Uri.parse(base_url + '/login'),
-      body: body
-  );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 201 CREATED response,
-    // then parse the JSON.
-    return response.body;
-  } else {
-    throw Exception('Failed HTTP REQUEST');
-  }
+  return send_request(body, '/login', true);
 }
 
 Future<String> send_code_to_email(email) async {
-
   var body = jsonEncode({ 'email': email} );
-
-  final response = await http.post(
-      Uri.parse(base_url + '/send_code'),
-      body: body
-  );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 201 CREATED response,
-    // then parse the JSON.
-    return response.body;
-  } else {
-    throw Exception('Failed HTTP REQUEST');
-  }
+  return send_request(body, '/send_code', true);
 }
 
 Future<String> send_code_to_server(code) async {
-
   var body = jsonEncode({ 'code': code} );
-
-  final response = await http.post(
-      Uri.parse(base_url + '/send_code_to_server'),
-      body: body
-  );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 201 CREATED response,
-    // then parse the JSON.
-    return response.body;
-  } else {
-    throw Exception('Failed HTTP REQUEST');
-  }
+  return send_request(body, '/send_code_to_server', true);
 }
 
 Future<String> change_password_request(new_password) async {
-
   var body = jsonEncode({ 'new_password': new_password} );
-
-  final response = await http.post(
-      Uri.parse(base_url + '/change_password'),
-      body: body
-  );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 201 CREATED response,
-    // then parse the JSON.
-    return response.body;
-  } else {
-    throw Exception('Failed HTTP REQUEST');
-  }
+  return send_request(body, '/change_password', true);
 }
 
 Future<String> create_user_request(email, password) async {
-
   var body = jsonEncode({ 'email': email,
                           'password': password} );
-
-  final response = await http.post(
-      Uri.parse(base_url + '/create_user'),
-      body: body
-  );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 201 CREATED response,
-    // then parse the JSON.
-    return response.body;
-  } else {
-    throw Exception('Failed HTTP REQUEST');
-  }
+  return send_request(body, '/create_user', true);
 }
 
 Future<String> send_personal_info_request(name, gender, age) async {
-
   var body = jsonEncode({ 'name': name,
     'gender': gender,
     'age': age
   } );
-
-  final response = await http.post(
-      Uri.parse(base_url + '/send_personal_info'),
-      body: body
-  );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 201 CREATED response,
-    // then parse the JSON.
-    return response.body;
-  } else {
-    throw Exception('Failed HTTP REQUEST');
-  }
+  return send_request(body, '/send_personal_info', true);
 }
 
 Future<String> upload_photo_request(bio) async {
-
   var body = jsonEncode({ 'bio': bio} );
-
-  final response = await http.post(
-      Uri.parse(base_url + '/upload_photo_and_bio'),
-      body: body
-  );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 201 CREATED response,
-    // then parse the JSON.
-    return response.body;
-  } else {
-    throw Exception('Failed HTTP REQUEST');
-  }
+  return send_request(body, '/upload_photo_and_bio', true);
 }
 
